bc68b2c1ea98b701fc08825f9eb5b5c10c323ee8
Breno Azevedo <maddox.br@gmail.com>
2019-10-18 12:41:11 -0300

    * Mohal Metal Maker energy drain fixed, now -2000e (was -600) to produce 9.1 metal * Metal Maker energy drain fixed/updated, now -150e (was -75e) to produce 1 metal * Metal Maker build time doubled * Arm and Core Solar Collectors build time reduced by 30%

--- luarules/configs/maker_defs.lua
+++ luarules/configs/maker_defs.lua
@@ -1,17 +1,18 @@
 
-local AC0 = 1/70
-local AC1 = 1/64
-local AC2 = 1/58
+local AC0 = 1/200    -- 1 metal/tick (was 1/70)
+local AC1 = 1/250
+local AC2 = 1/220 --1/58
 
+-- Up to 'c' energy amount into 'e' ratio (eg.: c=1000, e=1/100, gives 10 metal)
 local convertCapacities = {
-	[UnitDefNames.armmakr.id]  = { c = (70), e = (AC0) }, 
-    [UnitDefNames.cormakr.id]  = { c = (70), e = (AC0) },
-    [UnitDefNames.armfmkr.id]  = { c = (70), e = (AC1) },
-    [UnitDefNames.corfmkr.id]  = { c = (70), e = (AC1) },
-    [UnitDefNames.armmmkr.id]  = { c = (600), e = (AC2) }, 
-    [UnitDefNames.cormmkr.id]  = { c = (600), e = (AC2) },
-    [UnitDefNames.armuwmmm.id] = { c = (650), e = (AC2) }, 
-    [UnitDefNames.coruwmmm.id] = { c = (650), e = (AC2) },
+    [UnitDefNames.armmakr.id]  = { c = (150), e = (AC0) }, --c=70, 80
+    [UnitDefNames.cormakr.id]  = { c = (150), e = (AC0) },
+    [UnitDefNames.armfmkr.id]  = { c = (200), e = (AC1) },
+    [UnitDefNames.corfmkr.id]  = { c = (200), e = (AC1) },
+    [UnitDefNames.armmmkr.id]  = { c = (2000), e = (AC2) }, --c=600
+    [UnitDefNames.cormmkr.id]  = { c = (2000), e = (AC2) },
+    [UnitDefNames.armuwmmm.id] = { c = (2000), e = (AC2) },
+    [UnitDefNames.coruwmmm.id] = { c = (2000), e = (AC2) },
 }
 
 return convertCapacities
