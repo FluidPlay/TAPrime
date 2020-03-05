cfd78bf6a50ddb62a0650f4938a766c89a626618
Breno Azevedo <maddox.br@gmail.com>
2020-03-04 22:20:58 -0300

    * Cannon damage type multiplier vs lightbot armor now 0.35 (was 0.45), vs heavybot 0.75 (was 0.6) * Thermo damage type multiplier vs defense 0.25 (was 1), vs lightveh now 0.22 (was 0.3) * Core Shadow (T1 bomber) metal cost now 300 (was 200) max health now 700 (was 480) * Improved UI Gfx Buttons padding

--- gamedata/configs/damagemultipliers.lua
+++ gamedata/configs/damagemultipliers.lua
@@ -39,14 +39,14 @@ local damageMultipliers = {
                 structure = 1,  resource = 1, 		defense = 0.275,defenseaa = 0.6,    commander = 1.33,
 	}
 ,
-	cannon={ 	lightbot = 0.5, supportbot = 0.25,  heavybot = 0.75,lightveh = 1.6, 	    supportveh = 2,        heavyveh = 1,
+	cannon={ 	lightbot = 0.5, supportbot = 0.25,  heavybot = 0.75,lightveh = 1.6, 	supportveh = 2,        heavyveh = 1,
                 lightair = 1.5, supportair = 2, 	heavyair = 1.25,lightship = 2,      supportship = 0.5,    heavyship = 1,
                 structure = 1,  resource = 1, 		defense = 1.2, 	defenseaa = 0.22, 	commander = 0.75,
 	}
 ,
-	thermo={ 	lightbot = 0.4, supportbot = 1.1, 	heavybot = 1.6, lightveh = 0.3,    supportveh = 1.25,      heavyveh = 1.4,
-                lightair = 1.3, supportair = 2.5,	heavyair = 1,   lightship = 2,     supportship = 0.5,     heavyship = 1.75,
-                structure = 0.75,resource = 0.45, 	defense = 1,    defenseaa = 0.5, 	commander = 0.25,
+	thermo={ 	lightbot = 0.4, supportbot = 1.1, 	heavybot = 1.6, lightveh = 0.22,    supportveh = 1.25,      heavyveh = 1.4,
+                lightair = 1.3, supportair = 2.5,	heavyair = 1,   lightship = 2,      supportship = 0.5,     heavyship = 1.75,
+                structure = 0.75,resource = 0.45, 	defense = 0.25, defenseaa = 0.5,    commander = 0.25,
 	}
 ,
 	siege={ 	lightbot = 0.4, supportbot = 0.85,  heavybot = 1.2,	lightveh = 1, 	    supportveh = 0.6,       heavyveh = 0.25,
