882849811735057e951e2da42ae3cdba45760361
Breno Azevedo <maddox.br@gmail.com>
2020-06-11 01:52:34 -0300

    * Laser damage type multiplier vs heavyveh (Tanks et al) armor now 0.2 (was 0.44), vs lightair armor now 1 (was 1.25) * Fixed core lab (buildoptions, morphing) * Core Wasp (Laser/EMP drone) max velocity now 3.55 (was 2.41) * Arm Banshee (Missile drone) max velocity now 3.11 (was 2.1)

--- gamedata/configs/damagemultipliers.lua
+++ gamedata/configs/damagemultipliers.lua
@@ -14,8 +14,8 @@ local damageMultipliers = {
                 structure = 0.75,resource = 0.25, 	defense = 0.18, defenseaa = 0.25,   commander = 0.3,
 	}
 ,
-    laser={     lightbot = 1.25,supportbot = 2,     heavybot = 0.9, lightveh = 0.72,    supportveh = 0.5,   heavyveh = 0.42,
-                lightair = 1.25,supportair = 0.5,   heavyair = 1,   lightship = 1.25,   supportship = 2,    heavyship = 0.5,
+    laser={     lightbot = 1.25,supportbot = 2,     heavybot = 0.9, lightveh = 0.72,    supportveh = 0.5,   heavyveh = 0.2,
+                lightair = 0.75,supportair = 0.5,   heavyair = 1,   lightship = 1.25,   supportship = 2,    heavyship = 0.5,
                 structure = 1.1,resource = 0.5,     defense = 0.7,  defenseaa = 0.25,   commander = 0.3,
     }
 ,
