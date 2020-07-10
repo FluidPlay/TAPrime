9d3b5ffd5ae9e3a2470860d0949bc819b1f461d3
Breno Azevedo <maddox.br@gmail.com>
2020-07-10 03:09:38 -0300

    * New gadget prevents units from getting stuck in the factory after being built, disabling their collision for about 2 seconds.

--- gamedata/configs/damagemultipliers.lua
+++ gamedata/configs/damagemultipliers.lua
@@ -44,7 +44,7 @@ local damageMultipliers = {
                 structure = 1,  resource = 1, 		defense = 0.9, 	defenseaa = 0.22, 	commander = 0.75,
 	}
 ,
-	thermo={ 	lightbot = 0.5, supportbot = 1.1, 	heavybot = 1.5, lightveh = 0.22,    supportveh = 1.25,      heavyveh = 1.4,
+	thermo={ 	lightbot = 1,   supportbot = 1.1, 	heavybot = 1.2, lightveh = 0.22,    supportveh = 1.25,      heavyveh = 1.4,
                 lightair = 1.3, supportair = 2.5,	heavyair = 1,   lightship = 2,      supportship = 0.5,     heavyship = 1.75,
                 structure = 0.75,resource = 0.45, 	defense = 0.25, defenseaa = 0.5,    commander = 0.25,
 	}
@@ -64,7 +64,7 @@ local damageMultipliers = {
                 structure = 2, resource = 1.25,	    defense = 1.5, 	defenseaa = 0.75,   commander = 0.3,
 	}
 ,
-	flak={ 		lightbot = 0.5, supportbot = 0.9, 	heavybot = 1.33,lightveh = 0.35,    supportveh = 0.75,  heavyveh = 0.275,
+	flak={ 		lightbot = 0.75,supportbot = 0.9, 	heavybot = 1.33,lightveh = 0.35,    supportveh = 0.75,  heavyveh = 0.275,
 				lightair = 1, 	supportair = 0.75,  heavyair = 0.4, lightship = 0.33,   supportship = 0.45,heavyship = 0.75,
                 structure = 1, 	resource = 0.25,	defense = 0.3, 	defenseaa = 0.8,    commander = 0.15,
 	}
---
+++ luarules/gadgets/unit_nostuck_after_built.lua
@@ -0,0 +1,47 @@
+function gadget:GetInfo()
+    return {
+        name = "Unit no-stuck after built",
+        desc = "Disables unit collision for a couple seconds right after it's built, to prevent it from getting stuck in the factory",
+        author = "MaDDoX",
+        date = "2020",
+        license = "Public Domain",
+        layer = 0,
+        enabled = true,
+    }
+end
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--local AirTransports = {
+--    [UnitDefNames["armatlas"].id] = true,
+--    [UnitDefNames["armdfly"].id] = true,
+--    [UnitDefNames["corvalk"].id] = true,
+--    [UnitDefNames["corseah"].id] = true,
+--}
+
+if not (gadgetHandler:IsSyncedCode()) then
+    return end
+
+local delay = 50 -- How many frames to keep the unit collision disabled
+local trackedUnits = {} -- { [unitID]=(number)enableframe, .. }
+
+--function gadget:UnitFromFactory(unitID, unitDefID, unitTeam, factID, factDefID, userOrders)
+
+function gadget:UnitFinished(unitID, unitDefID, teamID)
+    --Spring.Echo("Disabling collision for "..unitID)
+    Spring.SetUnitBlocking(unitID, false, false, true)
+    --isBlocking, isSolidObjectCollidable, isProjectileCollidable, isRaySegmentCollidable, crushable, blockEnemyPushing, blockHeightChanges
+    trackedUnits[unitID] = Spring.GetGameFrame() + delay
+end
+
+function gadget:GameFrame(n)
+    for unitID, frame in pairs(trackedUnits) do
+        if n >= frame then
+            --Spring.Echo("Enabling collision for "..unitID)
+            Spring.SetUnitBlocking(unitID, true, true, true, true,
+                    true, true, true)
+            trackedUnits[unitID]=nil
+        end
+    end
+end
+
