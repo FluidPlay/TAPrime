14ae218a5b26ffa426d1cd5848e5a498a021d422
Damgam <c_2_damiano@op.pl>
2020-09-17 10:33:43 +0200

    fixed scavenger units

--- gamedata/unitdefs_post.lua
+++ gamedata/unitdefs_post.lua
@@ -79,6 +79,26 @@ end
 --end
 
 -- handle unitdefs and the weapons they contain
+
+
+-- create scavenger units
+VFS.Include("gamedata/scavengers/unitdef_changes.lua")
+local scavengerUnitDefs = {}
+
+ for name,uDef in pairs(UnitDefs) do
+	 --local faction = string.sub(name, 1, 3)
+	 if not string.find(name, '_scav') then
+		 if customDefs[name] ~= nil then
+			 scavengerUnitDefs[name..'_scav'] = tableMergeSpecial(deepcopy(uDef), deepcopy(customDefs[name]))
+		 else
+			 scavengerUnitDefs[name..'_scav'] = deepcopy(uDef)
+		 end
+	 end
+ end
+ for name,ud in pairs(scavengerUnitDefs) do
+	 UnitDefs[name] = ud
+ end
+
 for name,ud in pairs(UnitDefs) do
   UnitDef_Post(name,ud)
   if ud.weapondefs then
@@ -93,24 +113,6 @@ for name,ud in pairs(UnitDefs) do
   if SaveDefsToCustomParams then
       SaveDefToCustomParams("UnitDefs", name, ud)    
   end
-end
-
--- create scavenger units
-VFS.Include("gamedata/scavengers/unitdef_changes.lua")
-local scavengerUnitDefs = {}
-
-for name,uDef in pairs(UnitDefs) do
-	local faction = string.sub(name, 1, 3)
-	if not string.find(name, '_scav') then
-		if customDefs[name] ~= nil then
-			scavengerUnitDefs[name..'_scav'] = tableMergeSpecial(deepcopy(uDef), deepcopy(customDefs[name]))
-		else
-			scavengerUnitDefs[name..'_scav'] = deepcopy(uDef)
-		end
-	end
-end
-for name,ud in pairs(scavengerUnitDefs) do
-	UnitDefs[name] = ud
 end
 
 ---- TAPrime ::
